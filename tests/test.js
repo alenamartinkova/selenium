8584b6fc95839afa9dc61753b7a27b0a64761ca8
alenamartinkova <martinkova.a@gmail.com>
2022-04-02 19:19:25 +0200

    register tests wip

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,8 @@
 const {By,Key,Builder} = require("selenium-webdriver");
 var assert = require('assert');
 require("chromedriver");
-/*
-describe('Buy notebook tests', function() {
+
+/*describe('Buy notebook tests', function() {
     const tests = [
         {count: '1', expected: "1 item(s) - $1,202.00"},
         {count: '0', expected: "0 item(s) - $0.00"},
@@ -86,8 +86,90 @@ describe('Login tests', function() {
     afterEach(async function() {
         await driver.quit();
     });
-})*/
+})
+*/
+describe('Register tests', function() {
+    const tests = [
+        //{name: 'test', lastname: 'test', email: 'test2022_2@test.test', password: "test123", password_confirm: 'test123', phone: '123456789', privacy: true},
+        {name: 'test', lastname: 'test', email: 'test2022_2@test.test', password: 'test123', password_confirm: 'test123', phone: '123456789', privacy: false},
+        {name: '', lastname: 'test', email: 'test2022_2@test.test', password: 'test123', password_confirm: 'test123', phone: '123456789', privacy: true},
+        {name: '', lastname: '', email: 'test2022_2@test.test', password: 'test123', password_confirm: 'test123', phone: '123456789', privacy: true},
+        {name: '', lastname: '', email: '', password: 'test123', password_confirm: 'test123', phone: '123456789', privacy: true},
+        {name: '', lastname: '', email: '', password: '', password_confirm: 'test123', phone: '123456789', privacy: true},
+        {name: '', lastname: '', email: '', password: '', password_confirm: 'test123', phone: '', privacy: true},
+        //{email: 'notexistingemail@test.test', password: "wrongpassword"}
+    ];
 
+    tests.forEach(({name, lastname, email, password, password_confirm, phone, privacy}) => {
+        it('Register email: '+ email +', password: ' + password + ', password_confirm: '+ password_confirm + ', name: ' + name + ', lastname: ' + lastname + ', phone: ' + phone + ', privacy: ' + privacy, async function() {
+            try {
+                await driver.findElement(By.id('top-links')).findElement(By.css('.dropdown .dropdown-toggle')).click();
+                await driver.findElement(By.css('.dropdown-menu.dropdown-menu-right')).findElement(By.linkText('Register')).click();
+                await driver.findElement(By.id('input-firstname')).sendKeys(name);
+                await driver.findElement(By.id('input-lastname')).sendKeys(lastname);
+                await driver.findElement(By.id('input-email')).sendKeys(email);
+                await driver.findElement(By.id('input-password')).sendKeys(password);
+                await driver.findElement(By.id('input-confirm')).sendKeys(password_confirm);
+                await driver.findElement(By.id('input-telephone')).sendKeys(phone);
+
+                if (privacy) {
+                    await driver.findElement(By.css('input[name=agree]')).click();
+                }
+
+                await driver.findElement(By.css('input[type="submit"]')).click();
+
+                try {
+                    // element is displayed - register success
+                    let accountProfileDisplayed = await driver.findElement(By.id('common-success')).isDisplayed();
+                    assert.equal(true, accountProfileDisplayed);
+                } catch (err) {
+                    // register fail
+                    if (name == '' || name.length > 32) {
+                        let nameErrorDisplayed = await driver.findElement(By.xpath("//*[text()='First Name must be between 1 and 32 characters!']")).isDisplayed();
+                        assert.equal(true, nameErrorDisplayed);
+                    }
+
+                    if (lastname == '' || lastname.length > 32) {
+                        let lastnameErrorDisplayed = await driver.findElement(By.xpath("//*[text()='Last Name must be between 1 and 32 characters!']")).isDisplayed();
+                        assert.equal(true, lastnameErrorDisplayed);
+                    }
+
+                    if (email == '' || !checkRegex(email)) {
+                        let emailErrorDisplayed = await driver.findElement(By.xpath("//*[text()='E-Mail Address does not appear to be valid!']")).isDisplayed();
+                        assert.equal(true, emailErrorDisplayed);
+                    }
+
+                    if (phone == '' || phone.length < 3 || phone.length > 32) {
+                        let phoneErrorDisplayed = await driver.findElement(By.xpath("//*[text()='Telephone must be between 3 and 32 characters!']")).isDisplayed();
+                        assert.equal(true, phoneErrorDisplayed);
+                    }
+
+                    if (password == '' || password.length < 4 || password.length > 20) {
+                        let passwordErrorDisplayed = await driver.findElement(By.xpath("//*[text()='Password must be between 4 and 20 characters!']")).isDisplayed();
+                        assert.equal(true, passwordErrorDisplayed);
+                    }
+
+                    if (!privacy) {
+                        let privacyErrorDisplayed = await driver.findElement(By.css('.alert.alert-danger.alert-dismissible')).isDisplayed();
+                        assert.equal(true, privacyErrorDisplayed);
+                    }
+                }
+            }  catch (err) {
+                handleFailure(err, driver)
+            }
+        })
+    })
+
+    beforeEach(async function() {
+        driver = await new Builder().forBrowser("chrome").build();
+        //To wait for browser to build and launch properly
+        await driver.get("http://vsrvfeia0h-114.vsb.cz/opencart/");
+    });
+
+    afterEach(async function() {
+        await driver.quit();
+    });
+})
 
 function handleFailure(err, driver) {
     console.error('Something went wrong!\n', err.stack, '\n');
@@ -97,4 +179,12 @@ function handleFailure(err, driver) {
 async function getText(driver, id) {
     const el = await driver.findElement(By.id(id));
     return el.getText();
+}
+
+function checkRegex(email) {
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
